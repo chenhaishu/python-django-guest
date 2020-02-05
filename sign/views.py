ed5a4dc1c6216a59bfcdbb37434f2461b34aa84f
tree <nevergiveup2010@yeah.net>
2020-02-05 17:55:30 +0800

    change to use templates for web display

--- sign/views.py
+++ sign/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+# def index(request):
+#    return HttpResponse("hello world!")
+
 def index(request):
-    return HttpResponse("hello world!")
+    return render(request, "index.html")
